7493b423ad2e5ce036ce737d6f8f5b425d1241c3
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-21 22:54:00 +0100

    columns created

---
+++ database/migrations/2023_02_21_181133_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->string('orario_di_partenza', 5);
+            $table->string('orario_di_arrivo', 5);
+            $table->smallInteger('codice_treno')->unsigned();
+            $table->smallInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
